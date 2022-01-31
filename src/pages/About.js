3c4456e2cadfb2bc90470d6decf9677b93bd2c39
KasunGunawardana <kasun4it@gmail.com>
2022-02-01 02:40:22 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,7 +17,9 @@ const About = () => {
               <h1>Want to learn a little about me?</h1>
             </div>
             <div>
-            <p>I'm Kasun Gunawardana. I enjoy learning new skills and creating things that live on the internet. My interest in web development started back in 2017 while I was working for a company called Trade Promoters as an ERP administrator. They had a requerement for a small system to schedule/assign their technicians for each jobs and keep track on them. They also needed to revamp their existing outdated website. Since I come with an IT background, I knew some stuff related to web development. So I had to do those stuff while troubleshooting and resolving their ERP related problems. My knowledge of Web Development gradually increased ....</p>
+            <p className='about'>I'm Kasun Gunawardana. I enjoy learning new skills and creating things that live on the internet. My interest in web development started back in 2017 while I was working for a company called Trade Promoters as an ERP administrator. They had a requerement for a small system to schedule/assign their technicians for each jobs and keep track on them. They also needed to revamp their existing outdated website. Since I come with an IT background, I knew some stuff related to web development. So I had to do those stuff while troubleshooting and resolving their ERP related problems. My knowledge of Web Development gradually increased ....</p>
+            <p className='about'>So I joined ... Content coming soon..</p>
+            <div className="white-space-xl" style={{ height: '100px' }}></div>
             </div>
           </motion.div>
         </div>
