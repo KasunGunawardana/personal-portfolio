572d773498a3d2d63a6b2b66a4c76b30c2fef889
KasunGunawardana <kasun4it@gmail.com>
2022-04-23 11:32:59 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,7 +14,7 @@ const Banner = () => {
                     <span>Kasun Gunawardana.</span>
                   </div>
                   <div className="line sub">
-                    <span>I'm an experienced FrontEnd Developer with Backend development capabilities.</span>
+                    <span>I'm a Senior Web Developer with a bit of UI/UX Design experience.</span>
                   </div>
                 </h2>
                 <div className="btn-row">
